179ee3d510966871877de61b0600c1d3b93c8b46
Rama <mkrishna.se@gmail.com>
2024-07-07 16:34:25 +0100

    Added Switch case without default

--- src/main/java/com/edw/IndexController.java
+++ src/main/java/com/edw/IndexController.java
@@ -17,8 +17,32 @@ import java.util.HashMap;
 public class IndexController {
     @GetMapping("/")
     public HashMap helloWorld() {
-        if(true){
+
+        int day = 1;
+        switch (day) {
+        case 1:
+          System.out.println("Monday");
+          break;
+        case 2:
+          System.out.println("Tuesday");
+          break;
+        case 3:
+          System.out.println("Wednesday");
+          break;
+        case 4:
+          System.out.println("Thursday");
+          break;
+        case 5:
+          System.out.println("Friday");
+          break;
+        case 6:
+          System.out.println("Saturday");
+          break;
+        case 7:
+          System.out.println("Sunday");
+          break;
         }
+
         return new HashMap<>(){{
             put("Message", "Hello Spring Boot. Ini setelah commit");
         }};
